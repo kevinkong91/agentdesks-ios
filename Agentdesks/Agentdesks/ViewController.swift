2dff24c0b65d5d272c8c2943f125557a03705128
Kevin Kong <kevinkong91@gmail.com>
2018-05-05 14:19:02 -0700

    fix: add bar button connection

--- Agentdesks/Agentdesks/ViewController.swift
+++ Agentdesks/Agentdesks/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var sortButton: UIBarButtonItem!
     @IBOutlet var propertyCollectionView: PropertyCollectionView!
 
     override func viewDidLoad() {
