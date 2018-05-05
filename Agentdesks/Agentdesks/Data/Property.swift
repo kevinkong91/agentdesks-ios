04cfdedafecea56c2f233873fbad5aaa623cc01c
Kevin Kong <kevinkong91@gmail.com>
2018-05-05 13:06:52 -0700

    feat: data models

---
+++ Agentdesks/Agentdesks/Data/Property.swift
@@ -0,0 +1,18 @@
+//
+//  Property.swift
+//  Agentdesks
+//
+//  Created by Kevin Kong on 5/5/18.
+//  Copyright © 2018 Kevin Kong. All rights reserved.
+//
+
+import Foundation
+
+struct Property {
+    let photos: [URL]
+    let price: Double
+    let beds: Int
+    let baths: Float
+    let size: Float
+    let dateAdded: Date
+}
---
+++ Agentdesks/Agentdesks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Agentdesks
+//
+//  Created by Kevin Kong on 5/5/18.
+//  Copyright © 2018 Kevin Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+}
+
